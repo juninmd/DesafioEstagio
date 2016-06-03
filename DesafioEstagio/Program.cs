1500b2602c43bc40e153ffe2859e2730e6375a01
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-06-02 23:02:32 -0300

    Algoritmo pronto.

--- DesafioEstagio/Program.cs
+++ DesafioEstagio/Program.cs
@@ -10,6 +10,22 @@ namespace DesafioEstagio
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please, insert the value in minutes \n(Example: -10/30)");
+            var minutos = Console.ReadLine();
+
+            var data = DateTime.Now.AddMinutes(int.Parse(minutos));
+
+            Console.WriteLine($"{Cortesia(data)}! Today is {data.ToString("dd/MM/yyyy HH:mm:ss")}");
+            Console.ReadKey();
+        }
+
+        private static string Cortesia(DateTime data)
+        {
+            if (data.Hour > 6 && data.Hour < 12)
+                return "Good morning";
+            if (data.Hour >= 12 && data.Hour < 18)
+                return "Good afternoon";
+            return "Good evening";
         }
     }
 }
